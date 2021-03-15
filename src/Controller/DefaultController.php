e4bbb171e7c89a5cb31f4c9502378faa522209bb
ASelmane <47992754+ASelmane@users.noreply.github.com>
2021-03-15 23:05:13 +0100

    sports: Création de l'entitée sports,, affiche profils selon sport

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +13,30 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+    * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboard(UsersRepository $usersRepository)
+    {
+        $user = $this->getUser();
+        $user_id = $user->getId();
+        $user_sports = $user->getSport();
+
+        foreach ($user_sports as $sport) {
+            $sportUsers = $sport->getUsers();
+            foreach ($sportUsers as $users) {
+                if ($users->getId() !== $user_id) {
+                    $listUsers[] = $users;
+                }
+            }
+        }
+        $listUsers = array_unique($listUsers, SORT_REGULAR);
+        
+        return $this->render('default/dashboard.html.twig', [
+            'users' => $listUsers ,
         ]);
     }
 }
---
+++ src/Entity/Sports.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SportsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SportsRepository::class)
+ */
+class Sports
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Users::class, mappedBy="sport")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Users[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(Users $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->addSport($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(Users $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            $user->removeSport($this);
+        }
+
+        return $this;
+    }
+}
