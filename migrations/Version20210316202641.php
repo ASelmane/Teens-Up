38df92978d2a425f9f6410e8949541e22a952359
ASelmane <47992754+ASelmane@users.noreply.github.com>
2021-03-16 21:28:14 +0100

    master: Correction migration

--- migrations/Version20210316202641.php
+++ migrations/Version20210316202641.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210315175046 extends AbstractMigration
+final class Version20210316202641 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,6 +20,7 @@ final class Version20210315175046 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE sports (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_sports (users_id INT NOT NULL, sports_id INT NOT NULL, INDEX IDX_7C2E778C67B3B43D (users_id), INDEX IDX_7C2E778C54BBBFB7 (sports_id), PRIMARY KEY(users_id, sports_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE users_sports ADD CONSTRAINT FK_7C2E778C67B3B43D FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE users_sports ADD CONSTRAINT FK_7C2E778C54BBBFB7 FOREIGN KEY (sports_id) REFERENCES sports (id) ON DELETE CASCADE');
@@ -28,6 +29,8 @@ final class Version20210315175046 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users_sports DROP FOREIGN KEY FK_7C2E778C54BBBFB7');
+        $this->addSql('DROP TABLE sports');
         $this->addSql('DROP TABLE users_sports');
     }
 }
